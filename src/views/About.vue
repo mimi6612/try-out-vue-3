4630ef4b179106b9c67728d1c2a31e58ca11dd6f
ryoichiroSlogan <komatsuzaki.ryoichiro@slogan.jp>
2022-03-17 20:18:24 +0900

    about.vueにカウンターを追加

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="increment">
+      Count is: {{ state.count }}, double is: {{ state.double }}
+    </button>
   </div>
 </template>
+<script>
+import { reactive, computed } from "vue"
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0,
+      double: computed(() => state.count * 2)
+    })
+
+    function increment() {
+      state.count++
+    }
+
+    return {
+      state,
+      increment
+    }
+  }
+}
+</script>
